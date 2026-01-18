4cc868d135db1bfff0f486cf4896fa2578e6fae6
XiaJie <3113959631@qq.com>
2026-01-18 18:24:58 +0800

    Initial commit

---
+++ Assets/Script/GameBoot.cs
@@ -0,0 +1,11 @@
+using QFramework;
+using UnityEngine;
+
+public class GameBoot : MonoBehaviour
+{
+    void Start()
+    {
+        // ResKit.Init();
+        // UIKit.OpenPanel<TestPanel>();
+    }
+}
