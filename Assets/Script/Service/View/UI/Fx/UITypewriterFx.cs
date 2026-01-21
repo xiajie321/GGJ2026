0a69a50aea5b1dd77b857efad0b6920f74591815
XiaJie <3113959631@qq.com>
2026-01-21 13:36:59 +0800

    [更改] 规范命名空间

--- Assets/Script/Service/View/UI/Fx/UITypewriterFx.cs
+++ Assets/Script/Service/View/UI/Fx/UITypewriterFx.cs
@@ -1,19 +1,15 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.Events;
 
-namespace Script.Service.View.Component.Text
+namespace Script.Service.View.UI.Fx
 {
     /// <summary>
     /// 高级打字机组件：支持单个字符独立动画、富文本、闲置及退场动画。
     /// 富文本支持、简单接口、低性能消耗、依赖TMP。
     /// </summary>
     [RequireComponent(typeof(TMP_Text))]
-    public class TypewriterComponent : MonoBehaviour
+    public class UITypewriterFx : MonoBehaviour
     {
-        
     }
 }
