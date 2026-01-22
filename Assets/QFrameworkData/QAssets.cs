4c9483690aa16812f36f78681916e335ef66c3bf
XiaJie <3113959631@qq.com>
2026-01-22 18:42:20 +0800

    [测试] Ab打包测试

--- Assets/QFrameworkData/QAssets.cs
+++ Assets/QFrameworkData/QAssets.cs
@@ -11,9 +11,9 @@
 namespace QAssetBundle
 {
     
-    public class Testpanel_prefab
+    public class Uimessagetippanel_prefab
     {
-        public const string BundleName = "testpanel_prefab";
-        public const string TESTPANEL = "TestPanel";
+        public const string BundleName = "uimessagetippanel_prefab";
+        public const string UIMESSAGETIPPANEL = "UIMessageTipPanel";
     }
 }
