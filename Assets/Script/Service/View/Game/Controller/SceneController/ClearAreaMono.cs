aaed22d416736413fcffd621d4c4072c4f821e8a
XiaJie <3113959631@qq.com>
2026-01-25 18:12:19 +0800

    [新增] 碰撞矩阵

---
+++ Assets/Script/Service/View/Game/Controller/SceneController/ClearAreaMono.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Script.Service.View.Game.Controller.SceneController
+{
+    public class ClearAreaMono:MonoBehaviour
+    {
+        HashSet<Collider2D> values = new();
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            values.Add(other);
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            values.Remove(other);
+        }
+        /// <summary>
+        /// 清除碎块
+        /// </summary>
+        /// <returns></returns>
+        public int ClearArea()
+        {
+            int count = values.Count;
+            foreach (var value in values)
+            {
+                Destroy(value.gameObject);
+            }
+            return count;
+        }
+    }
+}
