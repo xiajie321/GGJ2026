baf830044e4ee50b95ad7b1fd41f340d3b7dcb0d
XiaJie <3113959631@qq.com>
2026-01-28 12:28:34 +0800

    [更改] 将状态单独分离为一个文件夹

---
+++ Assets/Script/Service/View/Game/Controller/State.cs
@@ -0,0 +1,38 @@
+﻿namespace Script.Service.View.Game.Controller
+{
+    public enum State//TODO 如有需要可以在这里补充状态
+    {
+        /// <summary>
+        /// 静止状态
+        /// </summary>
+        Idle,
+        /// <summary>
+        /// 移动状态
+        /// </summary>
+        Move,
+        /// <summary>
+        /// 跳跃状态
+        /// </summary>
+        Jump,
+        /// <summary>
+        /// 下落状态
+        /// </summary>
+        Whereabouts,
+        /// <summary>
+        /// 攻击状态
+        /// </summary>
+        Attack,
+        /// <summary>
+        /// 无敌状态
+        /// </summary>
+        Invincible,
+        /// <summary>
+        /// 死亡状态
+        /// </summary>
+        Dead,
+        /// <summary>
+        /// 攻击状态
+        /// </summary>
+        Injured,
+    }
+}
