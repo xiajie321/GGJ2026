b4bcd935b1a26142fe4df2ee587af36cfab62974
XiaJie <3113959631@qq.com>
2026-01-28 12:26:58 +0800

    [新增] 更改控制器结构使其结构更加松散

---
+++ Assets/Script/Service/View/Game/AbsControllerMono.cs
@@ -0,0 +1,29 @@
+﻿using QFramework;
+using Script.Service.View.Game.Controller;
+using UnityEngine;
+
+namespace Script.Service.View.Game
+{
+    public abstract class AbsControllerMono: MonoBehaviour
+    {
+        protected Animator _animation;//动画控制
+        protected Rigidbody2D _rigidbody2D;
+        protected Collider2D _collider2D;
+        protected Controller.Controller _controller;
+        protected FSM<State> _fsm = new();
+        protected void InitComponents()
+        {
+            _animation ??= GetComponent<Animator>();
+            _rigidbody2D ??= GetComponent<Rigidbody2D>();
+            _collider2D ??= GetComponent<Collider2D>();
+        }
+        public void SetController<T>(T controller)where T : Controller.Controller, new()//TODO 设置对应的控制器
+        {
+            _controller = controller;
+            _controller.SetAnimation(_animation);
+            _controller.SetRigidbody2D(_rigidbody2D);
+            _controller.SetFsm(_fsm);
+            _controller.SetCollider2D(_collider2D);
+        }
+    }
+}
--- Assets/Script/Service/View/Game/PlayerAbsControllerMono.cs
+++ Assets/Script/Service/View/Game/PlayerAbsControllerMono.cs
@@ -11,13 +11,8 @@ namespace Script.Service.View.Game
         SideView,//侧面观察模式
         TopDown//俯视角模式
     }
-    public class PlayerControllerMono : MonoBehaviour
+    public class PlayerAbsControllerMono : AbsControllerMono
     {
-        private Animator _animation;//动画控制
-        private Rigidbody2D _rigidbody2D;
-        private Collider2D _collider2D;
-        private Controller.Controller _controller;
-        private FSM<State> _fsm = new();
         [SerializeField]
         private PlayerControllerMode _mode;
         public PlayerControllerMode Mode
@@ -35,12 +30,21 @@ namespace Script.Service.View.Game
         public Collider2D Collider2D => _collider2D;
         private void Start()
         {
-            _animation ??= GetComponent<Animator>();
-            _rigidbody2D ??= GetComponent<Rigidbody2D>();
-            _collider2D ??= GetComponent<Collider2D>();
+            InitComponents();
             ModeSwitch();//尽量让这个方法在Start中的位置靠后,以免出现赋值为空的情况
         }
-
+        private void Update()
+        {
+            _fsm.Update();
+        }
+        private void FixedUpdate()
+        {
+            _fsm.FixedUpdate();
+        }
+        private void OnGUI()
+        {
+            _fsm.OnGUI();
+        }
         private void ModeSwitch()//TODO 控制器切换
         {
             switch (_mode)
@@ -55,25 +59,6 @@ namespace Script.Service.View.Game
                     SetController(new PlayerSideViewController());
                     break;
             }
-        }
-
-        private void Update()
-        {
-            _fsm.Update();
-        }
-
-        private void FixedUpdate()
-        {
-            _fsm.FixedUpdate();
-        }
-
-        public void SetController<T>(T controller)where T : Controller.Controller, new()//TODO 设置对应的控制器
-        {
-            _controller = controller;
-            _controller.SetAnimation(_animation);
-            _controller.SetRigidbody2D(_rigidbody2D);
-            _controller.SetFsm(_fsm);
-            _controller.SetCollider2D(_collider2D);
         }
     }
 }
