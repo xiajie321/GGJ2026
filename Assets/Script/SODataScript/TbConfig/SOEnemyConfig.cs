87ed8a001c330a3582bfce27b51e4ee9eb1059a5
handsome-GZXX <3581544162@qq.com>
2026-01-31 00:03:33 +0800

    [修改]Enemy,Trap,Item配置表

--- Assets/Script/SODataScript/TbConfig/SOEnemyConfig.cs
+++ Assets/Script/SODataScript/TbConfig/SOEnemyConfig.cs
@@ -13,6 +13,11 @@ namespace Script.SODataScript.TbConfig
                 Id = id,
                 Name = _ls.Name,
                 RuntimeAnimatorController = _ls.RuntimeAnimatorController,
+                MoveSpeed = _ls.MoveSpeed,
+                LoveType = _ls.LoveType,
+                Reword = _ls.Reword,
+                StandTime = _ls.StandTime,
+                ThinkTime = _ls.ThinkTime,
             };
         }
     }
@@ -22,5 +27,11 @@ namespace Script.SODataScript.TbConfig
         public int Id;//这里可以不用填,因为在Get方法中会返回
         public string Name = "";
         public RuntimeAnimatorController RuntimeAnimatorController;//动画控制器替换
+        public float MoveSpeed;//移动速度
+        public ItemType LoveType;//喜欢的物品类型
+        public float Reword;//发现喜爱的物品时获得的奖励
+        public float StandTime;//站立什么都不做的时间
+        public float ThinkTime;//对物品或陷阱进行思考的时间
     }
+    
 }
--- Assets/Script/SODataScript/TbConfig/SOItemConfig.cs
+++ Assets/Script/SODataScript/TbConfig/SOItemConfig.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Script.SODataScript.TbConfig
@@ -14,6 +15,11 @@ namespace Script.SODataScript.TbConfig
                 Id = id,
                 Name = _ls.Name,
                 Sprite = _ls.Sprite,
+                Type = _ls.Type,
+                Height = _ls.Height,
+                Level = _ls.Level,
+                Cost = _ls.Cost,
+                IsMoveable = _ls.IsMoveable,
             };
         }
     }
@@ -23,5 +29,50 @@ namespace Script.SODataScript.TbConfig
         public int Id;//这里可以不用填,因为在Get方法中会返回
         public string Name = "";
         public Sprite Sprite;
+
+        public ItemType Type;//物品类型
+
+        public Height Height;//物品的高度
+        public int Level;//遮挡等级
+        
+        public float Cost;//购买该物品的花费
+        
+        public bool IsMoveable;//物品是否可以移动
+        
+    }
+    
+    /// <summary>
+    /// 物品所需的类型
+    /// 目前只有一个Type类型,后续可以扩展
+    /// </summary>
+    [Serializable]
+    public enum ItemType
+    {
+        Type = 0,
+    }
+    /// <summary>
+    /// 物品能遮挡住的物体的等级
+    /// 高档物品一定能遮挡住低档漏洞
+    /// 低档物品一定不能遮挡住高档漏洞
+    /// </summary>
+    [Serializable]
+    public enum ItemLevel
+    {
+        S = 0,
+        A = 1,
+        B = 2,
+        F = 3,  //仅装饰用，无法挡住任何漏洞
+    }
+
+    /// <summary>
+    /// 物品以及漏洞的高度
+    /// 只有高度匹配的物品才能够吸附在对应高度的漏洞上
+    /// </summary>
+    [Serializable]
+    public enum Height
+    {
+        Wall = 0,//墙面
+        WallRoot = 1,//墙根
+        Ground = 2,//地面
     }
 }
