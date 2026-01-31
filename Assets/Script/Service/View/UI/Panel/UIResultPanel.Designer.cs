72bb87d633bb3c0d4e4b7f50d7c7c3afef492ef6
小桂花 <143058334+guihuala@users.noreply.github.com>
2026-01-31 14:57:00 +0800

    [新增] 拼一下结果面板

--- Assets/Script/Service/View/UI/Panel/UIResultPanel.Designer.cs
+++ Assets/Script/Service/View/UI/Panel/UIResultPanel.Designer.cs
@@ -5,16 +5,46 @@ using QFramework;
 
 namespace Service.View.UI.Panel
 {
-	// Generate Id:cbdd318a-42f8-47c4-913b-6431e2769c65
+	// Generate Id:96b50295-470e-4e7c-889e-5ecb6b17e010
 	public partial class UIResultPanel
 	{
 		public const string Name = "UIResultPanel";
 		
+		[SerializeField]
+		public UnityEngine.UI.Button CloseBtn;
+		[SerializeField]
+		public UnityEngine.UI.Image TitleImage;
+		[SerializeField]
+		public TMPro.TextMeshProUGUI ProfitText;
+		[SerializeField]
+		public TMPro.TextMeshProUGUI ScoreText;
+		[SerializeField]
+		public UnityEngine.UI.Button NextLevelBtn;
+		[SerializeField]
+		public UnityEngine.UI.Button TryAgainBtn;
+		[SerializeField]
+		public UnityEngine.UI.Button MainMenuBtn;
+		[SerializeField]
+		public UnityEngine.UI.Image UIResultStar_1;
+		[SerializeField]
+		public UnityEngine.UI.Image UIResultStar_2;
+		[SerializeField]
+		public UnityEngine.UI.Image UIResultStar_3;
 		
 		private UIResultPanelData mPrivateData = null;
 		
 		protected override void ClearUIComponents()
 		{
+			CloseBtn = null;
+			TitleImage = null;
+			ProfitText = null;
+			ScoreText = null;
+			NextLevelBtn = null;
+			TryAgainBtn = null;
+			MainMenuBtn = null;
+			UIResultStar_1 = null;
+			UIResultStar_2 = null;
+			UIResultStar_3 = null;
 			
 			mData = null;
 		}
