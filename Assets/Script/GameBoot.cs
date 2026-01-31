27b11526152e610b4a576c00f8b85f5ad86cd99a
hantaikeju <775846320@qq.com>
2026-01-31 21:37:46 +0800

    [修复]--相机和小地图异常问题修复

--- Assets/Script/GameBoot.cs
+++ Assets/Script/GameBoot.cs
@@ -18,6 +18,9 @@ public class GameBoot : MonoBehaviour,IController
         GameArchitecture.InitArchitecture();
         Debug.Log("[GameBoot] 游戏入口加载完毕");
         UIKit.OpenPanel<UIHomePanel>();
+        // this.GetSystem<LevelSystem>().StartLevel(0);
+        // this.GetSystem<ShopSystem>().InitShop();
+        // this.GetSystem<CameraEdgeScrollingSystem>().InitCameraSystem();
     }
 
     public IArchitecture GetArchitecture()
--- Assets/Script/Service/View/UI/Panel/UILoadingPanel.cs
+++ Assets/Script/Service/View/UI/Panel/UILoadingPanel.cs
@@ -1,54 +1,61 @@
-using UnityEngine;
-using UnityEngine.UI;
+using Cysharp.Threading.Tasks;
 using QFramework;
 using Script.Service.System;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace Service.View.UI.Panel
 {
-	public class UILoadingPanelData : UIPanelData
-	{
-	}
-	public partial class UILoadingPanel : UIPanel, ISceneSwitch,IController
-	{
-		public IArchitecture GetArchitecture()
-		{
-			return Script.Service.Architecture.GameArchitecture.Interface;
-		}
-		protected override void OnInit(IUIData uiData = null)
-		{
-			mData = uiData as UILoadingPanelData ?? new UILoadingPanelData();
-			// please add init code here
-		}
-		
-		protected override void OnOpen(IUIData uiData = null)
-		{
+    public class UILoadingPanelData : UIPanelData
+    {
+    }
+    public partial class UILoadingPanel : UIPanel, ISceneSwitch, IController
+    {
+        public IArchitecture GetArchitecture()
+        {
+            return Script.Service.Architecture.GameArchitecture.Interface;
+        }
+        protected override void OnInit(IUIData uiData = null)
+        {
+            mData = uiData as UILoadingPanelData ?? new UILoadingPanelData();
+            // please add init code here
+        }
 
-		}
-		
-		protected override void OnShow()
-		{
-		}
-		
-		protected override void OnHide()
-		{
-		}
-		
-		protected override void OnClose()
-		{
+        protected override void OnOpen(IUIData uiData = null)
+        {
 
-		}
+        }
 
-		public void OnLoad(float progress, bool isLoad)
-		{
-			Debug.Log($"正在加载... 进度: {progress * 100}%");
-		}
+        protected override void OnShow()
+        {
+        }
 
-		public bool OnLoadCompleted(float progress, bool isLoad)
-		{
-			Debug.Log("加载完成");
-			this.GetSystem<LevelSystem>().StartLevel(0); // 默认关卡
-			CloseSelf();
-			return true;
-		}
-	}
+        protected override void OnHide()
+        {
+        }
+
+        protected override void OnClose()
+        {
+
+        }
+
+        public void OnLoad(float progress, bool isLoad)
+        {
+            Debug.Log($"正在加载... 进度: {progress * 100}%");
+        }
+
+        public bool OnLoadCompleted(float progress, bool isLoad)
+        {
+            Debug.Log("加载完成");
+			StartInitCamera().Forget();
+            CloseSelf();
+            return true;
+        }
+
+        private async UniTask StartInitCamera()
+        {
+			await UniTask.Yield();
+            this.GetSystem<LevelSystem>().StartLevel(0); // 默认关卡
+        }
+    }
 }
