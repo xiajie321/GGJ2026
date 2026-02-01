4d106b0f4ee9be27c87a6d7ed38d4522f9a55205
小桂花 <143058334+guihuala@users.noreply.github.com>
2026-02-01 09:51:40 +0800

    [修复] 按钮选框

--- Assets/Script/Service/View/UI/Component/UISelection.cs
+++ Assets/Script/Service/View/UI/Component/UISelection.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using QFramework;
 using Service.View.UI.Panel;
 using UnityEngine;
@@ -8,15 +6,34 @@ using UnityEngine.UI;
 
 public static class UISelection
 {
+    class UISelectionCleaner : MonoBehaviour
+    {
+        private void OnDisable()
+        {
+            // 当按钮被隐藏、销毁、或场景切换导致物体失效时触发
+            // 只有当面板存在时才调用
+            var panel = UIKit.GetPanel<UICommonEffectPanel>();
+            if (panel != null)
+            {
+                panel.HideFrame();
+            }
+        }
+    }
+
     public static void BindGlobalSelectFrame(this Button button)
     {
+        // 检查是否已经添加过，防止重复添加
+        if (button.gameObject.GetComponent<UISelectionCleaner>() == null)
+        {
+            button.gameObject.AddComponent<UISelectionCleaner>();
+        }
+
         var trigger = button.gameObject.GetComponent<EventTrigger>() ?? button.gameObject.AddComponent<EventTrigger>();
         RectTransform rect = button.transform as RectTransform;
-        
+
         EventTrigger.Entry enter = new EventTrigger.Entry { eventID = EventTriggerType.PointerEnter };
         enter.callback.AddListener((data) =>
         {
-            // 通过 UIKit 获取全局特效面板并显示
             var panel = UIKit.GetPanel<UICommonEffectPanel>();
             if (panel != null)
             {
